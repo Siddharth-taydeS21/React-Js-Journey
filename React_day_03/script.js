2056324d25ca1fe5abc0e8dc53ef9630923d8f2d
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-07-01 23:31:01 +0530

    Day 3: Parcel Intro & deep dive

---
+++ React_day_03/script.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+console.log(React)
+
+const h1 = <h1>Hello Parcel</h1>
+
+const root =  ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(h1)
