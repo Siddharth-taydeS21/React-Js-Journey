146e91ae55711bcb7f4fc55116a8c90874ac7e59
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-08-01 22:13:56 +0530

    Day 7: Implemented fetch call using useEffect hook in React

--- React_day_07/components/ProductsContainer.jsx
+++ React_day_07/components/ProductsContainer.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-import products from "../products"
 
-export default function ProductsContainer({searchQuery}) {
+export default function ProductsContainer({ searchQuery }) {
+    const [products, setProducts] = useState([]);
     let newArray = [];
+
+    useEffect(() => {
+        // with the help of useEffect hook we can prevent this code from running repeatedly
+        fetch('https://6a50c67ec576c846dcb9db29.mockapi.io/SiddsOwnRestApi/products')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+
+        // this is the unmount call back function 
+        return () => {
+            console.log('Component Unmounted..!')
+        }
+    }, []) // we can pass any variables in this Dependency array for monitoring, on the variables change the code will run in the useEffect call back function
+
     const data = products;
     const filteredArray = data.forEach(product => {
-        const containsSubstring  =  product.search_tags.some(item => item.includes(searchQuery))
+        const containsSubstring = product.search_tags.some(item => item.includes(searchQuery))
         if (containsSubstring) {
             newArray.push(product)
         }
