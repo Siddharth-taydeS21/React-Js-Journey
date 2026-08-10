bc5be7fff9d4342319ee6f543dc91f4f9c8f8651
Siddharth Tayde <taydesiddharth999@gmail.com>
2026-08-10 23:33:04 +0530

    Deleted unnecessary files

--- app.js
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import AppCounter from "./components/AppCounter";
-
-// const root = createRoot(document.getElementById('root'))
-
-export default function App ({root}) {
-  return <AppCounter root={root} />
-}
--- components/button.js
+++
@@ -1,5 +0,0 @@
-export default function Button({imageUrl, title, clickEventHandler}){
-    return <button className="btn" title={title} onClick={clickEventHandler}>
-        <img src={imageUrl} alt="image" />
-    </button>
-}
